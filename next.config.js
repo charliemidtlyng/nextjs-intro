b89ac484284402a95527e25fbd6a93b39cadbec6
Charlie Midtlyng <Charlie.Midtlyng@BEKK.no>
2019-03-10 18:03:01 +0100

    Endre config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  target: 'serverless',
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
-
-    return config
-  }
+  target: 'serverless'
 }
